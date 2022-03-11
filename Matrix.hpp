44ff67a4af6757fbfc44c8f063c2e02c5266ab8e
ImreKorf <I.korf@outlook.com>
2022-03-11 12:35:50 +0100

    Added Doxygen style comments to the Matrix.hpp

--- Matrix.hpp
+++ Matrix.hpp
@@ -26,6 +26,13 @@ SOFTWARE.
 #include "Vec.hpp"
 #include <iostream>
 
+/**
+ * @brief Base matrix class
+ * 
+ * @tparam H height of the matrix
+ * @tparam W width of the matrix
+ * @tparam T datatype of the matrix
+ */
 template<unsigned int H, unsigned int W = H, typename T = double>
 class MatBase {
 private:
@@ -46,7 +53,6 @@ public:
 	void SetColumn(const unsigned int i, const Vec<H, T> V);
 	MatBase<H, W, T> Transpose(void) const;
 
-	template<int W2, typename N> 		MatBase<H, W2, T>& operator*=(const MatBase<W, W2, N> &B);		// Matrix multiplication
 	template<typename N>		 		MatBase<H, W , T>& operator*=(const MatBase<H, W , N> &B);		// Matrix multiplication 
 	template<int W2, typename N> const	MatBase<H, W2, T>  operator* (const MatBase<W, W2, N> &B) const;// Matrix multiplication
 	template<typename N> 		const	MatBase<H, W , T>  operator* (const MatBase<H, W , N> &B) const;// Matrix multiplication
@@ -59,6 +65,9 @@ public:
 
 	template<typename N> 		 const 	Vec<H, T> 	   	   operator* (const Vec<W, N> &V) 	 	  const;// Vector matrix multiplication
 	template<typename Cast> 		   					   operator MatBase<H, W, Cast> () 		  const;
+	/**
+	 * @brief prints out the values of the matrix
+	 */
 	void print(){
 		for(unsigned int i = 0; i < H; i++){
 			for(unsigned int j = 0; j < W; j++){
@@ -71,11 +80,26 @@ public:
 
 //! Data manipulation
 
+/**
+ * @brief Construct a new empty MatBase object
+ * 
+ * @tparam H height of the matrix
+ * @tparam W width of the matrix
+ * @tparam T datatype of the matrix
+ */
 template<unsigned int H, unsigned int W, typename T>
 MatBase<H, W, T>::MatBase() {
 	static_assert(std::is_arithmetic<T>::value, "Values must be numeric");
 }
 
+/**
+ * @brief Copy constructor
+ * 
+ * @tparam H height of the matrix
+ * @tparam W width of the matrix
+ * @tparam T datatype of the matrix
+ * @param m1 the MatBase object to copy from
+ */
 template<unsigned int H, unsigned int W, typename T>
 MatBase<H, W, T>::MatBase(const MatBase<H, W, T> &m1) {
 	for(unsigned int i = 0; i < H; i++){
@@ -83,6 +107,14 @@ MatBase<H, W, T>::MatBase(const MatBase<H, W, T> &m1) {
 	}
 }
 
+/**
+ * @brief Construct a new MatBase object based on an initializer list
+ * 
+ * @tparam H height of the matrix
+ * @tparam W width of the matrix
+ * @tparam T datatype of the matrix
+ * @param v the initializer list
+ */
 template<unsigned int H, unsigned int W, typename T>
 MatBase<H, W, T>::MatBase(std::initializer_list<std::initializer_list<T>> v){
 	static_assert(std::is_arithmetic<T>::value, "Values must be numeric");
@@ -110,16 +142,39 @@ MatBase<H, W, T>::MatBase(std::initializer_list<std::initializer_list<T>> v){
 	}
 }
 
+/**
+ * @brief Destroy the MatBase object
+ * 
+ * @tparam H height of the matrix
+ * @tparam W width of the matrix
+ * @tparam T datatype of the matrix
+ */
 template<unsigned int H, unsigned int W, typename T>
 MatBase<H, W, T>::~MatBase() {}
 
-//* Returns the row at position i
+/**
+ * @brief Returns the row at given position
+ * 
+ * @tparam H height of the matrix
+ * @tparam W width of the matrix
+ * @tparam T datatype of the matrix
+ * @param i the row position
+ * @return Vec<W, T> vector containing the row
+ */
 template<unsigned int H, unsigned int W, typename T>
 Vec<W, T> MatBase<H, W, T>::GetRow(const unsigned int i) const{
 	return (*this)[i];
 }
 
-//* Returns the column at position i
+/**
+ * @brief Returns the column at given position
+ * 
+ * @tparam H height of the matrix
+ * @tparam W width of the matrix
+ * @tparam T datatype of the matrix
+ * @param i the column position
+ * @return Vec<H, T> vector containing the column
+ */
 template<unsigned int H, unsigned int W, typename T>
 Vec<H, T> MatBase<H, W, T>::GetColumn(const unsigned int i) const{
 	Vec<H, T> C;
@@ -127,20 +182,43 @@ Vec<H, T> MatBase<H, W, T>::GetColumn(const unsigned int i) const{
 	return C;
 }
 
-//* Sets the row at position i
+/**
+ * @brief Set the row of the matrix
+ * 
+ * @tparam H height of the matrix
+ * @tparam W width of the matrix
+ * @tparam T datatype of the matrix
+ * @param i the row position
+ * @param V the vector of values
+ */
 template<unsigned int H, unsigned int W, typename T>
 void MatBase<H, W, T>::SetRow(const unsigned int i, const Vec<W, T> V){
 	(*this)[i] = V;
 }
 
-//* Sets the column at position i
+/**
+ * @brief Sets the column of the matrix
+ * 
+ * @tparam H height of the matrix
+ * @tparam W width of the matrix
+ * @tparam T datatype of the matrix
+ * @param i the column position
+ * @param V the vector of values
+ */
 template<unsigned int H, unsigned int W, typename T>
 void MatBase<H, W, T>::SetColumn(const unsigned int i, const Vec<H, T> V){
 	for(int j = 0; j < H; (*this)[j][i] = V[j], j++){}
 }
 
 //Math
-//* Transposes the matrix
+/**
+ * @brief Transposes the matrix
+ * 
+ * @tparam H height of the matrix
+ * @tparam W width of the matrix
+ * @tparam T datatype of the matrix
+ * @return MatBase<H, W, T> The transposed matrix
+ */
 template<unsigned H, unsigned int W, typename T>
 MatBase<H, W, T> MatBase<H, W, T>::Transpose(void) const{
 	MatBase<W, H, T> Mt;
@@ -152,17 +230,16 @@ MatBase<H, W, T> MatBase<H, W, T>::Transpose(void) const{
 
 //! Operator overloads
 
-//* Matrix multiplication
-template<unsigned int H, unsigned int W, typename T>
-template<int W2, typename N> 		
-MatBase<H, W2, T>& MatBase<H, W, T>::operator*=(const MatBase<W, W2, N> &B){
-	static_assert(W != W2, "Cannot shrink or expand size of matrix");
-	MatBase<H, W2, T> C = *this * B;
-	*this = C;
-	return *this;
-}
-
-//* Matrix multiplication
+/**
+ * @brief Multiplies the matrix with another inplace
+ * 
+ * @tparam H height of the matrix
+ * @tparam W width of the matrix
+ * @tparam T datatype of the matrix
+ * @tparam N datatype of the 2nd matrix
+ * @param B the 2nd matrix
+ * @return MatBase<H, W, T>& reference to the multiplied matrix object
+ */
 template<unsigned int H, unsigned int W, typename T>	
 template<typename N>
 MatBase<H, W, T>& MatBase<H, W, T>::operator*=(const MatBase<H, W, N> &B){
@@ -173,6 +250,17 @@ MatBase<H, W, T>& MatBase<H, W, T>::operator*=(const MatBase<H, W, N> &B){
 
 
 //* Matrix multiplication
+/**
+ * @brief Multiplies the matrix with another, allows for different matrix sizes
+ * 
+ * @tparam H height of the matrix
+ * @tparam W width of the matrix
+ * @tparam T datatype of the matrix
+ * @tparam W2 width of the 2nd matrix
+ * @tparam N datatype of the 2nd matrix
+ * @param B the 2nd matrix
+ * @return const MatBase<H, W2, T> the multiplied matrix object
+ */
 template<unsigned int H, unsigned int W, typename T>
 template<int W2, typename N> 		
 const MatBase<H, W2, T> MatBase<H, W, T>::operator*(const MatBase<W, W2, N> &B) const{
@@ -189,6 +277,16 @@ const MatBase<H, W2, T> MatBase<H, W, T>::operator*(const MatBase<W, W2, N> &B) 
 }
 
 //* Matrix multiplication
+/**
+ * @brief Multiplies the matrix with another
+ * 
+ * @tparam H height of the matrix
+ * @tparam W width of the matrix
+ * @tparam T datatype of the matrix
+ * @tparam N datatype of the 2nd matrix
+ * @param B the 2nd matrix
+ * @return const MatBase<H, W, T> the multiplied matrix object
+ */
 template<unsigned int H, unsigned int W, typename T>	
 template<typename N>	
 const MatBase<H, W, T> MatBase<H, W, T>::operator*(const MatBase<H, W, N> &B) const{
@@ -204,7 +302,16 @@ const MatBase<H, W, T> MatBase<H, W, T>::operator*(const MatBase<H, W, N> &B) co
 	return C;
 }
 
-//* Matrix Vector multiplication
+/**
+ * @brief Multiplies a vector to a matrix
+ * 
+ * @tparam H height of the matrix
+ * @tparam W width of the matrix
+ * @tparam T datatype of the matrix
+ * @tparam N datatype of the vector
+ * @param V the vector to multiply with
+ * @return const Vec<H, T> 
+ */
 template<unsigned int H, unsigned int W, typename T>
 template<typename N>
 const Vec<H, T> MatBase<H, W, T>::operator*(const Vec<W, N> &V) const{
@@ -216,7 +323,16 @@ const Vec<H, T> MatBase<H, W, T>::operator*(const Vec<W, N> &V) const{
 	return ColA;
 }
 
-//* Matrix scalar multiplication
+/**
+ * @brief Scales matrix inplace
+ * 
+ * @tparam H height of the matrix
+ * @tparam W width of the matrix
+ * @tparam T datatype of the matrix
+ * @tparam N datatype of the scalar
+ * @param r the scalar
+ * @return MatBase<H, W , T>& reference to the scaled matrix
+ */
 template<unsigned int H, unsigned int W, typename T>
 template<typename N>		 		
 MatBase<H, W , T>& MatBase<H, W, T>::operator*=(const N &r){
@@ -226,14 +342,32 @@ MatBase<H, W , T>& MatBase<H, W, T>::operator*=(const N &r){
 	return *this;
 }
 
-//* Matrix scalar multiplication
+/**
+ * @brief Scales matrix
+ * 
+ * @tparam H height of the matrix
+ * @tparam W width of the matrix
+ * @tparam T datatype of the matrix
+ * @tparam N datatype of the scalar
+ * @param r the scalar
+ * @return const MatBase<H, W , T> the scaled matrix
+ */
 template<unsigned int H, unsigned int W, typename T>
 template<typename N>		 		
 const MatBase<H, W , T> MatBase<H, W, T>::operator*(const N &r) const{
 	return MatBase<H, W, T>(*this) *= r;
 }
 
-//* Matrix addition
+/**
+ * @brief Adds two matrices together inplace
+ * 
+ * @tparam H height of the matrix
+ * @tparam W width of the matrix
+ * @tparam T datatype of the matrix
+ * @tparam N datatype of the 2nd matrix
+ * @param B the 2nd matrix
+ * @return MatBase<H, W , T>& reference to the matrix
+ */
 template<unsigned int H, unsigned int W, typename T>
 template<typename N>		 		
 MatBase<H, W , T>& MatBase<H, W, T>::operator+=(const MatBase<H, W, N> &B){
@@ -243,14 +377,32 @@ MatBase<H, W , T>& MatBase<H, W, T>::operator+=(const MatBase<H, W, N> &B){
 	return *this;
 }
 
-//* Matrix addition
+/**
+ * @brief Adds two matrices together
+ * 
+ * @tparam H height of the matrix
+ * @tparam W width of the matrix
+ * @tparam T datatype of the matrix
+ * @tparam N datatype of the 2nd matrix
+ * @param B the 2nd matrix
+ * @return const MatBase<H, W , T> the resulting matrix
+ */
 template<unsigned int H, unsigned int W, typename T>
 template<typename N>		 		
 const MatBase<H, W , T> MatBase<H, W, T>::operator+(const MatBase<H, W, N> &B) const{
 	return MatBase<H, W, T>(*this) += B;
 }
 
-//* Matrix subtraction
+/**
+ * @brief Subtracts matrix B from the matrix inplace
+ * 
+ * @tparam H height of the matrix
+ * @tparam W width of the matrix
+ * @tparam T datatype of the matrix
+ * @tparam N datatype of the 2nd matrix
+ * @param B the 2nd matrix
+ * @return MatBase<H, W , T>& reference to the matrix
+ */
 template<unsigned int H, unsigned int W, typename T>
 template<typename N>		 		
 MatBase<H, W , T>& MatBase<H, W, T>::operator-=(const MatBase<H, W, N> &B){
@@ -260,16 +412,34 @@ MatBase<H, W , T>& MatBase<H, W, T>::operator-=(const MatBase<H, W, N> &B){
 	return *this;
 }
 
-//* Matrix subtraction
+/**
+ * @brief Subtracts matrix B from the matrix
+ * 
+ * @tparam H height of the matrix
+ * @tparam W width of the matrix
+ * @tparam T datatype of the matrix
+ * @tparam N datatype of the 2nd matrix
+ * @param B the 2nd matrix
+ * @return const MatBase<H, W , T> the resulting matrix
+ */
 template<unsigned int H, unsigned int W, typename T>
 template<typename N>		 		
 const MatBase<H, W , T> MatBase<H, W, T>::operator-(const MatBase<H, W, N> &B) const{
 	return MatBase<H, W, T>(*this) -= B;
 }
 
+/**
+ * @brief Casts matrix to different matrix type
+ * 
+ * @tparam H height of the matrix
+ * @tparam W width of the matrix
+ * @tparam T datatype of the matrix
+ * @tparam Cast the new datatype of the matrix
+ * @return MatBase<H, W, Cast> the casted matrix
+ */
 template<unsigned int H, unsigned int W, typename T>
 template<typename Cast>
-MatBase<H, W, T>::operator MatBase<H, W, Cast> () 		  const {
+MatBase<H, W, T>::operator MatBase<H, W, Cast> () const {
 	MatBase<H, W, Cast> CM;
 	for(unsigned int i = 0; i < H; CM[i] = static_cast<Vec<W, Cast>>((*this)[i]), i++){}
 	return CM;
@@ -281,7 +451,13 @@ MatBase<H, W, T>::operator MatBase<H, W, Cast> () 		  const {
 //?			Matrix Class		 ?//
 //? 							 ?//
 
-// non-square matrix
+/**
+ * @brief Matrix class for a non-square matrix
+ * 
+ * @tparam H height of the matrix
+ * @tparam W width of the matrix
+ * @tparam T datatype of the matrix
+ */
 template<unsigned int H, unsigned int W = H, typename T= double>
 class Mat : public MatBase<H, W, T> {
 public:
@@ -308,7 +484,12 @@ public:
 	template<typename Cast> 		   				   operator Mat<H, W, Cast> () 	  	  const {return (Mat<H, W, Cast>)((MatBase<H, W, T>)(*this));}
 };
 
-// Square matrix
+/**
+ * @brief Specialized Square matrix
+ * 
+ * @tparam H height and width of the matrix
+ * @tparam T datatype of the matrix
+ */
 template<unsigned H, typename T>
 class Mat<H, H, T> : public MatBase<H, H, T> {
 public:
@@ -341,15 +522,23 @@ public:
 
 //! Math
 
-//* Get Submatrix of Matrix
+/**
+ * @brief Gets a submatrix from the square matrix
+ * copies the matrix to a new matrix object with [H-1] size and excludes passed row and column
+ * @tparam H height and width of the matrix
+ * @tparam T datatype of the matrix
+ * @param i the row to skip
+ * @param j the column to skip
+ * @return Mat<H-1, H-1, T> submatrix of the original matrix
+ */
 template<unsigned H, typename T>
 Mat<H-1, H-1, T> Mat<H, H, T>::SubMatrix(const unsigned int i, const unsigned int j) const {
 	Mat<H-1, H-1, T> Ms;
 	for(unsigned int i2 = 0, i3 = 0; i2 < H; i2++){
-		if(i2 == i)
+		if(i2 == i) // skip row
 			continue;
 		for(unsigned int j2 = 0, j3 = 0; j2 < H; j2++){
-			if(j2 == j)
+			if(j2 == j) // skip column
 				continue;
 			Ms[i3][j3] = (*this)[i2][j2];
 			j3++;
@@ -359,16 +548,41 @@ Mat<H-1, H-1, T> Mat<H, H, T>::SubMatrix(const unsigned int i, const unsigned in
 	return Ms;
 }
 
+/**
+ * @brief Calculates the Minor of the matrix
+ * 
+ * @tparam H height and width of the matrix
+ * @tparam T datatype of the matrix
+ * @param i the row to skip on the submatrix
+ * @param j the column to skip on the submatrix
+ * @return double the Minor of the matrix
+ */
 template<unsigned H, typename T>
 double Mat<H, H, T>::Minor(const unsigned int i, const unsigned int j) const {
 	return this->SubMatrix(i, j).Determinant();
 }
 
+/**
+ * @brief Calculates the CoFactor of the matrix
+ * 
+ * @tparam H height and width of the matrix
+ * @tparam T datatype of the matrix
+ * @param i the row to skip on the submatrix
+ * @param j the column to skip on the submatrix
+ * @return double the CoFactor of the matrix
+ */
 template<unsigned H, typename T>
 double Mat<H, H, T>::CoFactor(const unsigned int i, const unsigned int j) const {
 	return ((i+j) % 2 ? -1 : 1) * Minor(i, j);
 }
 
+/**
+ * @brief Calculates the Determinant of the matrix
+ * 
+ * @tparam H height and width of the matrix
+ * @tparam T datatype of the matrix
+ * @return double the Determinant of the matrix
+ */
 template<unsigned H, typename T>
 double Mat<H, H, T>::Determinant(void) const {
 	// gaussian elimination
@@ -397,6 +611,13 @@ double Mat<H, H, T>::Determinant(void) const {
 	return det;
 }
 
+/**
+ * @brief Calculates the cofactor matrix of the matrix
+ * 
+ * @tparam H height and width of the matrix
+ * @tparam T datatype of the matrix
+ * @return Mat<H, H, T> The cofactor matrix
+ */
 template<unsigned H, typename T>
 Mat<H, H, T> Mat<H, H, T>::CoFactorMatrix(void) const {
 	Mat<H, H, T> cM;
@@ -408,21 +629,40 @@ Mat<H, H, T> Mat<H, H, T>::CoFactorMatrix(void) const {
 	return cM;
 }
 
+/**
+ * @brief Calculates the Adjoint of the matrix
+ * 
+ * @tparam H height and width of the matrix
+ * @tparam T datatype of the matrix 
+ * @return Mat<H, H, T> the Adjointed matrix
+ */
 template<unsigned H, typename T>
 Mat<H, H, T> Mat<H, H, T>::Adjoint(void) const {
 	return CoFactorMatrix().Transpose();
 }
 
+/**
+ * @brief Calculates the inverse of the matrix
+ * 
+ * @throw runtime_error when matrix is not invertible
+ * @tparam H height and width of the matrix
+ * @tparam T datatype of the matrix
+ * @return Mat<H, H, T> The inversed matrix
+ */
 template<unsigned H, typename T>
 Mat<H, H, T> Mat<H, H, T>::Inverse(void) const {
 	double det = Determinant();
 	if(det == 0){
-		throw "Matrix is not invertible, determinant of 0";
+		throw std::runtime_error("Matrix is not invertible, determinant of 0");
 	}
 	return Adjoint() * (1 / det);
 }
 
-// recursion base case
+/**
+ * @brief Base case for recursion
+ * 
+ * @tparam T datatype of the matrix
+ */
 template<typename T>
 class Mat<1, 1, T> : public MatBase<1, 1, T> {
 
@@ -447,11 +687,25 @@ public:
 	double Determinant(void) const;
 };
 
+/**
+ * @brief base case for submatrix recursion
+ * 
+ * @tparam T the submatrix
+ * @param i the row to skip ( ignored )
+ * @param j the column to skip ( ignored ) 
+ * @return Mat<1, 1, T> the original matrix
+ */
 template<typename T>
 Mat<1, 1, T> Mat<1, 1, T>::SubMatrix(const unsigned int i, const unsigned int j) const {
 	return *this;
 }
 
+/**
+ * @brief Returns the value of [0,0] in the matrix
+ * This specialisation is needed for calculating the determinant
+ * @tparam T the datatype of the matrix
+ * @return double the determinant value
+ */
 template<typename T>
 double Mat<1, 1, T>::Determinant(void) const {
 	return (*this)[0][0];
